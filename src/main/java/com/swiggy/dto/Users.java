6cc902838a5968b4e9db3aad8fff742fe70c89dd
manu h <agashmanu@gmail.com>
2025-01-08 11:36:56 +0530

    user dto

---
+++ src/main/java/com/swiggy/dto/Users.java
@@ -0,0 +1,25 @@
+package com.swiggy.dto;
+
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Users {
+
+	private Integer userId;
+	private  String name;
+	private   String email;
+	private String password;
+	private String phoneNumber;
+	private  String address;
+	private Timestamp createdAt;
+
+}
