323fd0288e4f82c3fe9862f0a8d6441bb99d116d
sammed <sammeddigraje2002@gmail.com>
2025-01-08 15:50:19 +0530

    menuItems add

--- src/main/java/com/swiggy/dao/MenuItemDAO.java
+++ src/main/java/com/swiggy/dao/MenuItemDAO.java
@@ -1,10 +1,11 @@
 package com.swiggy.dao;
 
+import com.swiggy.dto.MenuItem;
+
 public interface MenuItemDAO {
-    void addMenuItem(MenuItem menuItem) throws SQLException;
-    MenuItem getMenuItem(int itemId) throws SQLException;
-    List<MenuItem> getAllMenuItems() throws SQLException;
-    void updateMenuItem(MenuItem menuItem) throws SQLException;
-    void deleteMenuItem(int itemId) throws SQLException;
-}
+	    void addMenuItem(MenuItem menuItem)  ;
+	   
+	}
+
+
 
